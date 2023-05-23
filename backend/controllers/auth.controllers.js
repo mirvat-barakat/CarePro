aeb57e394d99c2ed1cba241636b0535d965572d8
unknown <mirvatbarakat1@gmail.com>
2023-05-23 20:12:22 +0300

    backend<> Added login API

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,7 @@
 const User = require("../models/userModel")
 const jwt = require("jsonwebtoken");
 
-exports.register = async (req, res) => {
+  exports.register = async (req, res) => {
     const { name, email, password, role } = req.body;
   
     const existingUser = await User.findOne({ email });
@@ -18,4 +18,19 @@ exports.register = async (req, res) => {
     await user.save();
     const { password: hashedPassword, ...newUser } = user.toJSON()
     res.status(201).json(newUser);
+  }
+
+  exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+  
+    if (!user) return res.status(404).json({ message: "Invalid Credentials" });
+  
+    const isMatched = user.matchPassword(password);
+    if (!isMatched) return res.status(404).json({ message: "Invalid Credentials" });
+  
+    const token = jwt.sign({ id: user._id, email: user.email }, process.env.SECRET_KEY);
+  
+    res.json({ token })
+  
   }
