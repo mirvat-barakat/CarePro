689b87116bbc6b2a34640c5d6bbf048699048cd4
unknown <mirvatbarakat1@gmail.com>
2023-05-23 20:10:47 +0300

    backend<> Added register API

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,21 @@
+const User = require("../models/userModel")
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+    const { name, email, password, role } = req.body;
+  
+    const existingUser = await User.findOne({ email });
+  
+    if (existingUser) return res.status(409).json({ message: "Email already exists" })
+  
+    const user = new User();
+    user.name = name;
+    user.email = email;
+    user.password = password;
+  
+  
+    if (role) user.role = role;
+    await user.save();
+    const { password: hashedPassword, ...newUser } = user.toJSON()
+    res.status(201).json(newUser);
+  }
