c0c9a28ad92289e112dadbd5d16300a5dee95ff0
unknown <mirvatbarakat1@gmail.com>
2023-05-24 14:02:00 +0300

    frontend<> Added log in component into landing page

---
+++ frontend/src/Pages/LandingPage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../../Components/Login";
+
+
+const LandingPage = () => {
+
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LandingPage;
