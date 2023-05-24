7075b00dfc318af45ea5019b28a671ac4933a0f2
unknown <mirvatbarakat1@gmail.com>
2023-05-24 14:06:49 +0300

    frontend<> Added routes to landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
+import LandingPage from '././Pages/LandingPage';
+import { Routes, Route, Router } from "react-router-dom";
 
 function App() {
+    <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="*" element={<div>404</div>} />
+  </Routes>
 }
 
 export default App;
