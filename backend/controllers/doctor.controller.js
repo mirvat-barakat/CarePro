b806d13ae10f7d5a7bd389799be6bf9dcee0a0f3
unknown <mirvatbarakat1@gmail.com>
2023-05-26 21:15:35 +0300

    backend<> Created API that will add doctor notes into patients data

--- backend/controllers/doctor.controller.js
+++ backend/controllers/doctor.controller.js
@@ -5,3 +5,35 @@ exports.getPatients = async (req, res) => {
 	const patients = await User.find({ role: 'patient' });
 	res.json(patients);
 };
+
+const Patient = require('./models/Patient');
+
+exports.addDoctorNote = async (req, res) => {
+  const { user_id } = req.params;
+  const { medications, note, message } = req.body;
+
+  try {
+    const patient = await Patient.findOne({ user_id });
+
+    if (!patient) {
+      return res.status(404).json({ error: 'Patient not found' });
+    }
+
+    const doctorNote = {
+      date: new Date(),
+      medications: medications || [],
+      note: note || '',
+      message: message || ''
+    };
+
+    patient.doctorNotes.push(doctorNote);
+    await patient.save();
+
+    res.json({
+         message: 'Doctor note added successfully',
+         patient: patient
+     });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to add doctor note' });
+  }
+};
