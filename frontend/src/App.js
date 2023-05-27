bffc780fb8debea75c263a8cf13bd0866951cb6b
unknown <mirvatbarakat1@gmail.com>
2023-05-27 13:45:37 +0300

    backend<> Fixed a problem in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './Pages/LandingPage';
-import { Routes, Route, Router } from "react-router-dom";
+
 
 function App() {
-  <Router>
+  return(
+    <Router>
      <Routes>
        <Route path="/" element={<LandingPage />} />
        <Route path="*" element={<div>404</div>} />
      </Routes>
-
-  </Router>
+    </Router>
+  );
+  
  }  
 
 export default App;
