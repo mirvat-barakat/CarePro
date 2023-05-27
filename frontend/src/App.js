cb1242bc013b9d4b374a17f4dbc68e64c4a5decb
unknown <mirvatbarakat1@gmail.com>
2023-05-27 16:57:53 +0300

    frontend<>Added route for login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './Pages/LandingPage';
-
+import LoginPage from './Pages/LoginPage';
 
 function App() {
   return(
     <Router>
      <Routes>
        <Route path="/" element={<LandingPage />} />
-       <Route path="*" element={<div>404</div>} />
+       <Route path="/" element={<LandingPage />} />
+       <Route path="/login" element={<LoginPage />} />
      </Routes>
     </Router>
   );
