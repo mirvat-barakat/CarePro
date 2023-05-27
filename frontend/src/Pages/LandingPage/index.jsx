d1102a32d64b4407dda991e6b1720e36663fb68f
unknown <mirvatbarakat1@gmail.com>
2023-05-27 14:13:16 +0300

    frontend<>Added landing page structure

--- frontend/src/Pages/LandingPage/index.jsx
+++ frontend/src/Pages/LandingPage/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Login from "../../Components/Login";
-
+import "./styles.css";
 
 const LandingPage = () => {
 
   return (
-    <div>
-      <Login />
+    <div className="main">
+      <div className="text-container">
+        <h1>Title</h1>
+        <p>Some text content goes here...</p>
+      </div>
+      <img src={"backgroundimage.jpg"} alt="Background" />
     </div>
   );
 };
