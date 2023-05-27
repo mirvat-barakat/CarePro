e03068b8bf28f245b22fe5ca7f7453b928a89758
unknown <mirvatbarakat1@gmail.com>
2023-05-27 22:07:44 +0300

    frontend<> Updated login page

--- frontend/src/Pages/LoginPage/index.jsx
+++ frontend/src/Pages/LoginPage/index.jsx
@@ -6,8 +6,25 @@ import "./styles.css";
 const LoginPage = () => {
 
   return (
-    <div>
-        <Login />
+    <div className="background">
+        <div className="block">
+        <div className="image-select">
+          <div className="image-select-left">
+            <img src="d.svg" alt="Left Image" />
+          </div>
+          <div className="image-select-middle">
+            <div className="option-box patient" >
+              <h2>Patients</h2>
+            </div>
+            <div className="option-box doctor" >
+              <h2>Doctors</h2>
+            </div>
+          </div>
+          <div className="image-select-right">
+            <img src="patients.jpg" alt="Right Image" />
+          </div>
+        </div>
+        </div>
     </div>
   );
 };
