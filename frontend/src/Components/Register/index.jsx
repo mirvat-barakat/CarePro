0e7121903b5e52e48bb9bb9158a84e734bcd1add
unknown <mirvatbarakat1@gmail.com>
2023-05-27 21:01:03 +0300

    frontend<> Updated register block component

--- frontend/src/Components/Register/index.jsx
+++ frontend/src/Components/Register/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import "./styles.css";
+import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
 const RegisterBlock = () => {
@@ -20,6 +21,7 @@ const RegisterBlock = () => {
                     <label htmlFor="password">Password:</label>
                     <input type="password" id="password" name="password" required />
                 </div>
+                <p>Already Have an Account? <Link to="/login">Log In Here</Link></p>
                 <button type="submit">Register</button>
             </form>
         </div>
--- frontend/src/Pages/LoginPage/index.jsx
+++ frontend/src/Pages/LoginPage/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "../../Components/Login";
+import Register from "../../Components/Register";
 import "./styles.css";
 
 const LoginPage = () => {
