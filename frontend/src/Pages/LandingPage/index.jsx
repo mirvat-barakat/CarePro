0a7f54a19b8dbbadb3222d809c1af093ae7345b9
unknown <mirvatbarakat1@gmail.com>
2023-05-27 16:52:07 +0300

    frontend<>Added navigation to login page

--- frontend/src/Pages/LandingPage/index.jsx
+++ frontend/src/Pages/LandingPage/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import Login from "../../Components/Login";
+import { useNavigate } from "react-router-dom";
 import "./styles.css";
 
 const LandingPage = () => {
+
+
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    navigate("/login");
+  }
 
   return (
     <div className="main">
@@ -10,7 +17,7 @@ const LandingPage = () => {
         <img src={"button.png"} alt="Background" className="icon"/> 
         <p className="h1">Your Health is our priority</p>
         <p className="quote">"Medicine is not only a science; it is also an art. It does not consist of compounding pills and plasters; it deals with the very processes of life, which must be understood before they may be guided." - Paracelsus</p>
-        <button>Get Started</button>
+        <button onAuxClick={handleClickButton}>Get Started</button>
       </div>
       <img src={"backgroundimage.jpg"} alt="Background" />
     </div>
