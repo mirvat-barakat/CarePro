d95875bd151a7cb1a29f0ce1a5a3aa4b1380230c
unknown <mirvatbarakat1@gmail.com>
2023-05-27 14:41:03 +0300

    frontend<>Created login page and added login component to it

---
+++ frontend/src/Pages/LoginPgae/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../../Components/Login";
+import "./styles.css";
+
+const LoginPage = () => {
+
+  return (
+    <div>
+        <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
