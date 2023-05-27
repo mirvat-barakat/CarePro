55703234a84d5c26bb36ac80789812f237b2f10e
unknown <mirvatbarakat1@gmail.com>
2023-05-27 13:17:28 +0300

    backend<> Updated index.js file

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,19 @@ require("dotenv").config();
 app.use(express.json())
 app.use(cors());
 
+
+const { authMiddleware } = require("./middlewares/auth.middleware");
+const { patientMiddleware } = require("./middlewares/patient.middleware");
+const { doctorMiddleware } = require("./middlewares/doctor.middleware");
+
 const authRouter = require("./routes/auth.routes")
-app.use('/auth', authRouter)
+app.use('/auth', authMiddleware, authRouter)
+
+const patientRouter = require("./routes/patient.routes")
+app.use('/patient', patientMiddleware, patientRouter)
+
+const doctorRouter = require("./routes/doctor.routes")
+app.use('/doctor', doctorMiddleware, doctorRouter)
 
 
 app.listen(process.env.PORT, (err) => {
