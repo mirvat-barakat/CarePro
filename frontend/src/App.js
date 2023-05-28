f973b1333c4efeb5e18dc96cbb85fa7d207ebe10
unknown <mirvatbarakat1@gmail.com>
2023-05-28 21:48:10 +0300

    frontend<> Added nescessary pages and routes for them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './Pages/LandingPage';
 import LoginPage from './Pages/LoginPage';
-import LoginP from './Pages/LoginP';
+import PatientsPage from './Pages/PatientsPage';
+import DoctorsPage from './Pages/DoctorsPage';
 
 function App() {
   return(
     <Router>
      <Routes>
-       <Route path="/" element={<LoginP />} />
-       {/* <Route path="/" element={<LandingPage />} /> */}
+       <Route path="/" element={<LandingPage />} />
        <Route path="/login" element={<LoginPage />} />
+       <Route path="/patients" element={<PatientsPage />} />
+       <Route path="/doctors" element={<DoctorsPage />} />
      </Routes>
     </Router>
   );
