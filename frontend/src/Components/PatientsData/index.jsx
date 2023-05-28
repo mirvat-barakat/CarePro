8a8f3b325ee97642989405e32401cb34be3e840a
unknown <mirvatbarakat1@gmail.com>
2023-05-28 19:56:26 +0300

    frontend<> Added structure for add patients data form

--- frontend/src/Components/PatientsData/index.jsx
+++ frontend/src/Components/PatientsData/index.jsx
@@ -3,8 +3,68 @@ import "./styles.css";
 
 const PatientsForm = () => {
 
+    const [formData, setFormData] = useState({
+        user_id: '',
+        dateOfBirth: '',
+        address: '',
+        allergies: '',
+        medications: '',
+        preExistingConditions: '',
+        pastSurgeries: '',
+        familyMedicalHistory: '',
+        notes: '',
+      });
+    
+      const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({ ...prevData, [name]: value }));
+      };
+
   return (
-    <></>
+    <>
+         <div>
+      <h2>ICE Patients Data Form</h2>
+      <form >
+        <div>
+          <label>User ID:</label>
+          <input type="text" name="user_id" value={formData.user_id} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Date of Birth:</label>
+          <input type="date" name="dateOfBirth" value={formData.dateOfBirth} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Address:</label>
+          <input type="text" name="address" value={formData.address} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Allergies:</label>
+          <input type="text" name="address" value={formData.allergies} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Medications:</label>
+          <input type="text" name="address" value={formData.medications} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Pre Existing Conditions:</label>
+          <input type="text" name="address" value={formData.preExistingConditions} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Past Surgeries:</label>
+          <input type="text" name="address" value={formData.pastSurgeries} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Family Medical History:</label>
+          <input type="text" name="address" value={formData.familyMedicalHistory} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label>Notes:</label>
+          <input type="text" name="address" value={formData.notes} onChange={handleInputChange} required />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+    </>
   );
 };
 
--- frontend/src/Pages/LoginP/index.jsx
+++ frontend/src/Pages/LoginP/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Login from "../../Components/Login";
 import Register from "../../Components/Register";
+import PatientsForm from "../../Components/PatientsData";
 import "./styles.css";
 
 const LoginP = () => {
 
   return (
-    <Register />
+    <PatientsForm />
   );
 };
 
