f6ec67e2a1d00d1a12ce9cc8da135eb267f5dd01
unknown <mirvatbarakat1@gmail.com>
2023-05-28 17:07:09 +0300

    backend<> Updated login form

--- frontend/src/Components/Login/index.jsx
+++ frontend/src/Components/Login/index.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React , {useState} from 'react';
 import "./styles.css";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 const LoginBlock = ({ onSubmit }) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const data = JSON.stringify({
+    "email": email,
+    "password": password
+   });
+
+   const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+  };
+
+  const handleLogin = async() => {
+
+    const config = {
+      method: "GET",
+      data:data,
+      url: 'http://localhost:3000/auth/login',
+      headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json',
+      },
+    };
+    try {
+      const res = await axios(config);
+      alert("axios");
+      if (res.data.status == "success" && res.data.user.role == "patient") {
+        alert("success");
+         localStorage.setItem("token", res.data.authorisation.token);
+
+      }
+    } catch (error) {
+      return error.response;
+    }
+
+  }
 
   const handleIconClick = () => {
     onSubmit();
@@ -18,15 +60,15 @@ const LoginBlock = ({ onSubmit }) => {
       <form >
         <div className="form-group">
           <label htmlFor="email" >Email:</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" name="email" value={email}  onChange={handleEmailChange}/>
         </div>
         <div className="form-group">
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
+          <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange}/>
         </div>
         <p>Don't Have an Account? <Link to="">Sign Up Here</Link></p>
       </form>
-      <button type="submit">Login</button>
+      <button type="submit" onClick={handleLogin}>Login</button>
     </div>
   );
 };
