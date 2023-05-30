0455504fa6054e06667270d59299abe14d8795a0
unknown <mirvatbarakat1@gmail.com>
2023-05-30 07:04:49 +0300

    backend<> Updated login route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -3,7 +3,7 @@ const router = Router();
 
 const { login, register } = require("../controllers/auth.controllers")
 
-router.get("/login", login);
+router.post("/login", login);
 router.post("/register", register);
 
 module.exports = router;
