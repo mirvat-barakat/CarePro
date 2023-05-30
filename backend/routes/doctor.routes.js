aabfb997e59229e8b4e7ebcffe040b5b6b0766e9
unknown <mirvatbarakat1@gmail.com>
2023-05-30 09:07:18 +0300

    backend<> Cleaned the code

--- backend/routes/doctor.routes.js
+++ backend/routes/doctor.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getPatients, getPatientProfile, addDoctorNote } = require("../controllers/doctor.controller")
+const { getPatients, getPatientProfile, addDoctorNote, sendEmail } = require("../controllers/doctor.controller")
 
 router.get("/getPatients", getPatients);
 router.get("/:id/getPatientProfile", getPatientProfile);
