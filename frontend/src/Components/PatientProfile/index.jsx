902e6005a446cf4697e357ab26ef1bf445ad30d6
unknown <mirvatbarakat1@gmail.com>
2023-06-01 06:15:24 +0300

    frontend<> Added logout for patients

--- frontend/src/Components/PatientProfile/index.jsx
+++ frontend/src/Components/PatientProfile/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import './styles.css';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const PatientProfileForm = ({ profile }) => {
+const PatientProfileForm = ({ profile, onClose  }) => {
 
   const [comments, setComments] = useState("");
   const [medications, setMedications] = useState("");
@@ -10,6 +12,7 @@ const PatientProfileForm = ({ profile }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [user_id, setId] = useState(localStorage.getItem("user_id"));
   const id = user_id.replace(/"/g, "");
+  const [idVisible, setIdVisible] = useState(true);
 
   const handleCommentsChange = (event) => {
     setComments(event.target.value);
@@ -25,6 +28,10 @@ const PatientProfileForm = ({ profile }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+  };
+
+  const handleIconClick = () => {
+    setIdVisible(false);
   };
 
   const handleSaveDoctorInformation = async() => {
@@ -57,64 +64,74 @@ const PatientProfileForm = ({ profile }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="form">
-      <div className="value">
-        <label>Address: </label>
-        <div>{profile.address}</div>
-      </div>
-      <div className="value">
-        <label>Date of Birth: </label>
-        <div>{profile.dateOfBirth}</div>
-      </div>
-      <div className="value">
-        <label>Medical Condition: </label>
-        <div>{profile.medicalcondition}</div>
-      </div>
-      <div className="value">
-        <label>Symptoms: </label>
-        <div>{profile.symptoms}</div>
-      </div>
-      <div className="value">
-        <label>allergies: </label>
-        <div>{profile.allergies}</div>
-      </div>
-      <div className="value">
-        <label>medications: </label>
-        <div>{profile.medications}</div>
-      </div >
-      <div className="value">
-        <label>preExistingConditions: </label>
-        <div>{profile.preExistingConditions}</div>
-      </div>
-      <div className="value">
-        <label>pastSurgeries:</label>
-        <div>{profile.pastSurgeries}</div>
-      </div>
-      <div className="value">
-        <label>familyMedicalHistory:</label>
-        <div>{profile.familyMedicalHistory}</div>
-      </div>
-      <div className="value">
-        <label>notes: </label>
-        <div>{profile.notes}</div>
-      </div>
-      <div className="doctor1">
-      <div>
-        <label>Comments:</label>
-        <textarea value={comments} onChange={handleCommentsChange}></textarea>
-      </div>
-      <div>
-        <label>Medications:</label>
-        <textarea value={medications} onChange={handleMedicationsChange}></textarea>
-      </div>
-      <div>
-        <label>Doctor's Notes:</label>
-        <textarea value={notes} onChange={handleNotesChange}></textarea>
-      </div>
-      </div>
+    <div >
+            {idVisible && (
+          <div >
+          <form onSubmit={handleSubmit} className="form">
+            <FontAwesomeIcon icon={faTimes} className="faicon1" onClick={handleIconClick}/>
+            <div className="value">
+              <label>Address: </label>
+              <div>{profile.address}</div>
+            </div>
+            <div className="value">
+              <label>Date of Birth: </label>
+              <div>{profile.dateOfBirth}</div>
+            </div>
+            <div className="value">
+              <label>Medical Condition: </label>
+              <div>{profile.medicalcondition}</div>
+            </div>
+            <div className="value">
+              <label>Symptoms: </label>
+              <div>{profile.symptoms}</div>
+            </div>
+            <div className="value">
+              <label>allergies: </label>
+              <div>{profile.allergies}</div>
+            </div>
+            <div className="value">
+              <label>medications: </label>
+              <div>{profile.medications}</div>
+            </div >
+            <div className="value">
+              <label>preExistingConditions: </label>
+              <div>{profile.preExistingConditions}</div>
+            </div>
+            <div className="value">
+              <label>pastSurgeries:</label>
+              <div>{profile.pastSurgeries}</div>
+            </div>
+            <div className="value">
+              <label>familyMedicalHistory:</label>
+              <div>{profile.familyMedicalHistory}</div>
+            </div>
+            <div className="value">
+              <label>notes: </label>
+              <div>{profile.notes}</div>
+            </div>
+            <div className="doctor1">
+            <div>
+              <label>Comments:</label>
+              <textarea value={comments} onChange={handleCommentsChange}></textarea>
+            </div>
+            <div>
+              <label>Medications:</label>
+              <textarea value={medications} onChange={handleMedicationsChange}></textarea>
+            </div>
+            <div>
+              <label>Doctor's Notes:</label>
+              <textarea value={notes} onChange={handleNotesChange}></textarea>
+            </div>
+            </div>
 
-      <button type="submit" onClick={handleSaveDoctorInformation}>Save</button>
-    </form>
+            <button type="submit" onClick={handleSaveDoctorInformation}>Save</button>
+            </form>
+          </div>
+        )}
+    </div>
+
+
+
   );
 };
 
--- frontend/src/Pages/PatientsPage/index.jsx
+++ frontend/src/Pages/PatientsPage/index.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PatientsForm from "../../Components/PatientsData";
+import Confirmation from "../../Components/ConfirmationDialog";
 import "./styles.css";
+import { useNavigate } from "react-router-dom";
 
 const LoginP = () => {
+  const [showLogoutDialog, setShowLogoutDialog] = useState(false);
+
+  const navigate = useNavigate();
+
+  function handleLogoutClick(){
+    setShowLogoutDialog(true);
+}
+
+function handleLogoutCancel() {
+    setShowLogoutDialog(false);
+}
+
+const handleLogout = () => {
+      
+  localStorage.clear();
+  navigate("/login");
+}
 
   return (
     <div className="ppage">
+      <button className="button" onClick={handleLogoutClick}>Logout</button>
       <PatientsForm />
+      {showLogoutDialog && (
+                <div className="add-form-backdrop">
+                            <Confirmation
+                            message="Are you sure you want to logout?"
+                            onCancel={handleLogoutCancel}
+                            onConfirm={handleLogout}
+                            /></div>)}
     </div>
     
   );
